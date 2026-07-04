6ea35852fcb2bcaa9ef86e19fb155788510bd154
jackvactus <caasigovners@gmail.com>
2026-07-04 14:09:26 +0000

    Optimize image display with better proportions

--- components/app-layout.tsx
+++ components/app-layout.tsx
@@ -67,7 +67,7 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
                 alt="Application logo"
                 width={40}
                 height={40}
-                className="h-10 w-10 object-contain"
+                className="h-10 w-10 object-cover object-center"
               />
             </div>
             <div>
